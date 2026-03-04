793009620d21387dd99a2c8fc422926d037acc6d
SoloMan1719 <ravicheepurupalli419@gmail.com>
2026-03-04 22:36:10 +0530

    Change Student entity to Students in controller

--- src/main/java/com/example/demo/Controller/StudentControler.java
+++ src/main/java/com/example/demo/Controller/StudentControler.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.Service.StudentService;
-import com.example.demo.entity.Student;
+import com.example.demo.entity.Students;
 
 @RestController
 @CrossOrigin("*")
@@ -18,7 +18,7 @@ public class StudentControler {
 
    
     @PostMapping("/create")
-    public Student saveStudent(@RequestBody Student student) {
+    public Students saveStudent(@RequestBody Students student) {
         return service.createStudent(student);
     }
 
