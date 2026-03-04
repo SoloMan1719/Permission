5021c20abaa2032169b9fc043bc387bebf43488d
SoloMan1719 <ravicheepurupalli419@gmail.com>
2026-03-04 22:49:38 +0530

    Rename StudentRepository to StudentsRepository

--- src/main/java/com/example/demo/Repository/StudentRepository.java
+++ src/main/java/com/example/demo/Repository/StudentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Students;
 
-public interface StudentRepository extends JpaRepository<Students, String>{
+public interface StudentsRepository extends JpaRepository<Students, String>{
 
 	Optional<Student> findById(String rollNumber);
 
