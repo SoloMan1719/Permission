1965477a12a59f9643cdea433e4ba199c1d62b5d
SoloMan1719 <ravicheepurupalli419@gmail.com>
2026-03-16 23:43:29 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/Controller/WardenController.java
@@ -0,0 +1,31 @@
+package com.example.demo.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.Service.StudentService;
+import com.example.demo.entity.Student;
+
+@RestController
+@RequestMapping("/Warden")
+@CrossOrigin("*")
+public class WardenController {
+	 @Autowired
+	    private StudentService service;
+
+	    @PutMapping("/approves/{rollNumber}")
+	    public Student approve(@PathVariable String rollNumber){
+	        return service.updateStatus(rollNumber, "APPROVED");
+	    }
+
+	    @PutMapping("/rejects/{rollNumber}")
+	    public Student reject(@PathVariable String rollNumber){
+	        return service.updateStatus(rollNumber, "REJECTED");
+	    }
+	    @GetMapping("/all")
+	    public List<Student> viewAllRequests(){
+	        return service.getAll();
+	    }
+	
+}
